3dd5d3f5d9f37bbaf2b509e90e6a97923b4dcee2
josephbill <josephbill00@gmail.com>
2024-11-26 15:41:02 +0300

    added notes on project dev. process

--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
+
 # Create your models here.
 class CustomUser(AbstractUser):
     # here you can add any custom field if necessary
