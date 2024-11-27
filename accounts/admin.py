b4efc9e365988fb892b14a1d0b5d42071b1ccc3f
josephbill <josephbill00@gmail.com>
2024-11-27 11:04:41 +0300

    added admin content for session at 3

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import CustomUser
+from django.contrib.auth.admin import UserAdmin
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ("username", "email", "is_staff", "is_active")
+    list_filter = ("is_staff", "is_active")
+    search_fields = ("username", "email")
+    ordering = ("username",)
--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Task
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("title", "completed")
+    list_filter = ("title", "completed")
+    search_fields = ("title", "user__username")
