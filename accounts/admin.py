3cfc5a2eeeee6983346b00c492a62bfe6944043f
josephbill <josephbill00@gmail.com>
2024-11-29 06:41:00 +0300

    added admin.py concepts to apps

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,13 +1,19 @@
 from django.contrib import admin
-
-# Register your models here.
 from .models import CustomUser
 from django.contrib.auth.admin import UserAdmin
 
+# Register your models here.
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
+    # model to map to
     model = CustomUser
-    list_display = ("username", "email", "is_staff", "is_active")
-    list_filter = ("is_staff", "is_active")
-    search_fields = ("username", "email")
+    # this maps to the of users shown 
+    list_display = ("username","email","is_staff","is_active","date_joined")
+    list_filter = ("is_staff","is_active")
+    #search filter
+    search_fields = ("username","email")
+    # list ordering 
     ordering = ("username",)
+    
+    # superuser]
+    # python manage.py createsuperuser
